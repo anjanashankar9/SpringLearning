47ddbb91d86fd3745e5241baecf788cce9cec821
Anjana Shankar <anjanashankar9@gmail.com>
2020-07-16 12:27:09 +0530

    Bean scope testing singleton and prototype

---
+++ src/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,20 @@
+package springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanscope-applicationContext.xml");
+
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        boolean result = (theCoach == alphaCoach);
+
+        System.out.println(result);
+
+        context.close();
+    }
+}
