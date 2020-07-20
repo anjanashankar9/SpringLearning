15a330d4dba4ae2c470a10f39757b5c1f108c3d9
Anjana Shankar <anjanashankar9@gmail.com>
2020-07-20 13:04:18 +0530

    Qualifier in case of multiple matching beans

--- src/springdemo_annotations/TennisCoach.java
+++ src/springdemo_annotations/TennisCoach.java
@@ -1,12 +1,14 @@
 package springdemo_annotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("thatTennisCoach")
 public class TennisCoach implements Coach {
 
     @Autowired
+    @Qualifier("happyFortuneService")
     private FortuneService fortuneService;
 
     /*@Autowired
