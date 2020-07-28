9a4ac52d153971d8338f09d321df7fb629ed5cbb
Anjana Shankar <anjanashankar9@gmail.com>
2020-07-28 22:40:36 +0530

    Random Fortune Service

--- src/springdemo_annotations/RandomFortuneService.java
+++ src/springdemo_annotations/RandomFortuneService.java
@@ -2,18 +2,25 @@ package springdemo_annotations;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomFortuneService implements FortuneService {
 
     private String[] data = {
             "Beware of the wolf in sheep's clothing",
-            "Diligence is the mother of good luck," +
+            "Diligence is the mother of good luck",
                     "The journey is the reward"
     };
+
+    //create a random number generator
+    private Random myRandom = new Random();
 
     @Override
     public String getFortune() {
         // pick a random string from the array
-        return "This is random fortune service";
+        int index = myRandom.nextInt(data.length);
+        String theFortune = data[index];
+        return theFortune;
     }
 }
