5186071f076bb43f60b0968e12af2eb3a5285429
Anjana Shankar <anjanashankar9@gmail.com>
2020-08-04 23:09:41 +0530

    Configuration through Java class

--- src/springdemo_annotations/JavaConfigDemoApp.java
+++ src/springdemo_annotations/JavaConfigDemoApp.java
@@ -1,14 +1,15 @@
 package springdemo_annotations;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AnnotaionDemoApp {
+public class JavaConfigDemoApp {
     public static void main(String[] args) {
 
         //read spring config file
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext(
-                        "springdemo_annotations/applicationContext.xml");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(
+                        SportConfig.class);
 
         //get the bean from spring container
         Coach theCoach = context.getBean("tennisCoach", Coach.class);
---
+++ src/springdemo_annotations/SportConfig.java
@@ -0,0 +1,11 @@
+package springdemo_annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("springdemo_annotations")
+public class SportConfig {
+
+
+}
